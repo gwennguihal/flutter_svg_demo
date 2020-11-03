73a4bb1cab81f1aa5bd7a7658adea52d0b29c2c1
Guihal Gwenn <gguihal@oui.sncf>
2020-11-03 11:03:19 +0100

    first commit

---
+++ android/app/src/main/kotlin/fr/myrddin/svg_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package fr.myrddin.svg_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SVG Lag Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      _preloadThenLaunch();
+    });
+  }
+
+  Future<void> _preloadThenLaunch() async {
+    final svgs = ['assets/onboarding_1.svg', 'assets/onboarding_2.svg'];
+    await Future.wait(svgs.map(_preloadSVG));
+    print('preloaded');
+    await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => OnBoarding1Screen(),
+      ),
+    );
+  }
+
+  Future<void> _preloadSVG(String path) {
+    print(path);
+    return precachePicture(
+      ExactAssetPicture(
+        SvgPicture.svgStringDecoder,
+        path,
+      ),
+      context,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PreLoading'),
+      ),
+      body: Center(
+        child: Text('preload...'),
+      ),
+    );
+  }
+}
+
+class OnBoarding1Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Center(
+          child: Column(children: [
+            SvgPicture.asset('assets/onboarding_1.svg'),
+            RaisedButton(
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => OnBoarding2Screen(),
+                ),
+              ),
+              child: Text('Next'),
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class OnBoarding2Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Center(
+          child: Column(children: [
+            SvgPicture.asset('assets/onboarding_2.svg'),
+          ]),
+        ),
+      ),
+    );
+  }
+}
